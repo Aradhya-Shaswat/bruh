c4d2c8d7417a92fe0aeb6bc2bb019dd766db436d
Arsh_DEV <100127769+Aradhya-Shaswat@users.noreply.github.com>
2023-07-15 20:44:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
       'media.discordapp.net',
       'instagram.fpat2-3.fna.fbcdn.net',
       'external.xx.fbcdn.net',
+      'https://encrypted-tbn0.gstatic.com',
     ],
   },
 };
