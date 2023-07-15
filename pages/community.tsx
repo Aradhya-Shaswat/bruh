7a0ea3be85a82636458989476d454b2699535287
Arsh_DEV <100127769+Aradhya-Shaswat@users.noreply.github.com>
2023-07-15 21:14:28 +0530

    Create community.tsx

---
+++ pages/community.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Community = () => {
+  return (
+    <div className='flex items-center justiy-center flex-col mt-[35vh] font-semibold text-2xl rounded mb-10 border-r-2
+    border-gray-100 p-3'>
+        <i>Community</i> Feature is Coming Soon to Friendbase!
+    </div>
+  )
+}
+
+export default Community
