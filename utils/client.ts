24ef4ebe8c40a3c9ecc769c01421b8cd7e76b792
Arsh_DEV <100127769+Aradhya-Shaswat@users.noreply.github.com>
2023-07-15 12:20:52 +0530

    Update client.ts

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 't6ctl7ok',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
