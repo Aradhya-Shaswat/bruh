77111681fdbab5806ceb55dc27249a6087c8e05a
Arsh_DEV <100127769+Aradhya-Shaswat@users.noreply.github.com>
2023-08-28 12:37:57 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,29 +16,31 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJT8QX90VQ"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-KJT8QX90VQ');
-      </script>
-    </Head>
-    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-        <Navbar />
-        <div className='flex gap-6 md:gap-20 '>
-          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-            <Sidebar />
-          </div>
-          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-            <Component {...pageProps} />
+    <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJT8QX90VQ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-KJT8QX90VQ');
+        </script>
+      </Head>
+      <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+        <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+          <Navbar />
+          <div className='flex gap-6 md:gap-20 '>
+            <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+              <Sidebar />
+            </div>
+            <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
-      </div>
-    </GoogleOAuthProvider>
+      </GoogleOAuthProvider>
+    </>
   );
 };
 
